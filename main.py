674d2d33f6002e37f051bb60502a519566e1df2f
gstamb <georgi.stamboliyski@university.mentormate.com>
2023-06-04 20:34:33 +0200

    Implement backend functionality to widgets

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QTextEdit, QLineEdit, QPushButton
+from backend import Chatbot
+import threading
 
 
 class ChatbotWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.chatbot = Chatbot()
 
         self.setMinimumSize(700, 500)
 
@@ -16,12 +19,26 @@ class ChatbotWindow(QMainWindow):
         # Add input field widget
         self.input_field = QLineEdit(self)
         self.input_field.setGeometry(10, 340, 480, 30)
+        self.input_field.returnPressed.connect(self.send_message)
 
         # # Add the button widget
         self.button = QPushButton("Send", self)
         self.button.setGeometry(500, 340, 50, 30)
+        self.button.clicked.connect(self.send_message)
 
         self.show()
+
+    def send_message(self):
+        user_input = self.input_field.text().strip()
+        self.chat_area.append(f"<p style='color:#333333'>User: {user_input}</p>")
+        self.input_field.clear()
+
+        thread = threading.Thread(target=self.get_bot_response, args=(user_input,))
+        thread.start()
+
+    def get_bot_response(self, user_input):
+        response = self.chatbot.get_response(user_input)
+        self.chat_area.append(f"<p style='color:#333333; background-color:#e9e9e9'>Bot: {response}</p>")
 
 
 app = QApplication(sys.argv)
